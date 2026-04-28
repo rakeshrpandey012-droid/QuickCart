187a512bf591bb3da752aa07db5f22767c6981ae
rakeshrpandey012-droid <rakeshrpandey012@gmail.com>
2026-04-28 21:43:25 +0530

    1st commit

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
 import User from "@/models/Users";
+import Order from "@/models/Order";
 
 export const inngest = new Inngest({ id: "quickcart-next" });
 
@@ -53,5 +54,35 @@ export const syncUserDeletion = inngest.createFunction(
         const { id } = event.data;
         await connectDB();
         await User.findByIdAndDelete(id);
+    }
+)
+
+// Inngest Function to create user's order in database
+export const createUserOrder = inngest.createFunction(
+    {
+        id: 'create-user-order',
+        batchEvents: {
+            maxSize: 25,
+            timeout: '5s'
+        }
+    },
+    {event: 'order/created' },
+    async ({events}) => {
+
+        const orders = events.map((event)=> {
+            return {
+                userId: event.data.userId,
+                items: event.data.items,
+                amount: event.data.amount,
+                address: event.data.address,
+                date : event.data.date
+            }
+        })
+
+        await connectDB()
+        await Order.insertMany(orders)
+
+        return { success: true, processed: orders.length };
+
     }
 )
