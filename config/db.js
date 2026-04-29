42da34a73455cb78fed883c1b221d56842cc588d
rakeshrpandey012-droid <rakeshrpandey012@gmail.com>
2026-04-29 12:11:23 +0530

    Finished

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ if (!cached) {
 }
 
 async function connectDB() {
+    
     if (cached.conn) {
         return cached.conn;
     }
@@ -16,7 +17,7 @@ async function connectDB() {
             bufferCommands: false,
         }
 
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts).then((mongoose) => { 
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then( mongoose => { 
             return mongoose;
         })
     
--- config/inngest.js
+++ config/inngest.js
@@ -16,8 +16,8 @@ export const syncUserCreation = inngest.createFunction(
         const { id, first_name, last_name, email_addresses, image_url } = event.data;
         const userData = {
             _id: id,
-            name: first_name + " " + last_name,
             email: email_addresses[0].email_address,
+            name: first_name + " " + last_name,
             imageUrl: image_url,
         }
         await connectDB();
